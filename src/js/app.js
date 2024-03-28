3a924f64c7bfe213d6ac89aca3350f6910dedf33
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-03-28 16:42:09 +0000

    cambios

--- src/js/app.js
+++ src/js/app.js
@@ -28,14 +28,30 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  if (variables.name == null || variables.lastName == null) {
+  if (variables.name == null && variables.lastName == null) {
+    variables.name = "Ingrese su nombre";
+    variables.lastName = "";
+  }
+  if (variables.name != null && variables.lastName == null) {
+    variables.name = "Ingrese su nombre";
+    variables.lastName = "";
+  }
+  if (variables.name == null && variables.lastName != null) {
     variables.name = "Ingrese su nombre";
     variables.lastName = "";
   }
   if (variables.role == null) {
     variables.role = "Sin rol asignado";
   }
-  if (variables.city == null || variables.country == null) {
+  if (variables.city == null && variables.country == null) {
+    variables.city = "Sin locaclización asignada";
+    variables.country = "";
+  }
+  if (variables.city != null && variables.country == null) {
+    variables.city = "Sin locaclización asignada";
+    variables.country = "";
+  }
+  if (variables.city == null && variables.country != null) {
     variables.city = "Sin locaclización asignada";
     variables.country = "";
   }
@@ -51,10 +67,10 @@ function render(variables = {}) {
           <h2>${variables.role}</h2>
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
